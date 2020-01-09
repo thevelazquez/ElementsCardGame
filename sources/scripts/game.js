933d8eed915965b72790d9bd66fc0740e3575a97
Edgardo Velazquez <edv9298@hotmail.com>
2020-01-09 00:49:37 -0500

    new player class, more game class

--- sources/scripts/game.js
+++ sources/scripts/game.js
@@ -1,6 +1,45 @@
-const Deck = require('./deck.js')
-let deck = new Deck();
+const Deck = require('./deck.js');
+const Player = require('./player.js');
+const readline = require('readline')
+const con = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
 
-console.log(deck.count());
-console.log(deck.draw());
-console.log(deck.count());
+class Game {
+	constructor() {
+		let deck = new Deck();
+		this.players = [];
+		this.setPlayers();
+		this.dealCards();
+		this.listPlayers();
+	}
+	setPlayers() {
+		let settingPlayers = true;
+		while (settingPlayers) {
+			let input = "";
+			if (input != "done") {
+				const player = new Player();
+				player.setName(input);
+				this.players.push(player);
+			} else {
+				settingPlayers = false;
+			}
+		}
+	}
+	listPlayers() {
+		console.log("The current players are: ");
+		for (let player of this.players) {
+			console.log(player.playerName + " holds " + player.showHand());
+		}
+	}
+	dealCards() {
+		for (let i = 0; i<7; i++) {
+			for (let player of this.players) {
+				player.draw(deck.draw())
+			}
+		}
+	}
+}
+
+let game = new Game();
