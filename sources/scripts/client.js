f1a066e4f17f1b5134bd662df2d6cd0c9610efdb
Edgardo Velazquez <edv9298@hotmail.com>
2020-01-11 23:15:33 -0500

    added ip variable

--- sources/scripts/client.js
+++ sources/scripts/client.js
@@ -1,4 +1,5 @@
-var socket = io.connect('http://localhost:4000');
+const ip = `25.9.154.97:4000`;
+let socket = io.connect(`http://${ip}`);
 socket.on('name', () => {
   let name = window.prompt("Enter your player name!", "Your name");
   socket.emit('name',name);
